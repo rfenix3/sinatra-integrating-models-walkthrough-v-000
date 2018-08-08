e256edce09bb70eacf1ea86b846531bee732e0af
Richard Fenix <richard.fenix@gmail.com>
2018-08-08 02:58:39 +0000

    automatically backed up by learn

--- models/text_analyzer.rb
+++ models/text_analyzer.rb
@@ -1,16 +1,16 @@
 # Your TextAnalyzer model code will go here.
 class TextAnalyzer
   attr_reader :text
- 
+
   def initialize(text)
     @text = text.downcase
   end
- 
+
   def count_of_words
     words = text.split(" ")
     words.count
   end
- 
+
   def count_of_vowels
     text.scan(/[aeoui]/).count
   end
@@ -36,8 +36,8 @@ class TextAnalyzer
         biggest = {}
         biggest[key] = value
       end
-    end  
+    end
     biggest
   end
-  
+
 end
